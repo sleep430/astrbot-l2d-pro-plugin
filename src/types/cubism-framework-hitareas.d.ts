d387fabbe291bc44fab8f48902cfebd3dbee81cb
sleep430 <3308665881@qq.com>
2026-07-01 01:20:39 -0500

    Remove Cubism hit area type shim

--- src/types/cubism-framework-hitareas.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CubismIdHandle } from '@cubism-framework/id/cubismid'
-
-declare module '@cubism-framework' {
-  interface ICubismModelSetting {
-    getHitAreaId(index: number): string & CubismIdHandle
-    getHitAreaName(index: number): string
-  }
-}
